bc3346469a23528d590f5cbbc1593c318fa1c62d
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2018-10-04 23:47:16 +0500

    Update caesar.c
    Added a problem description to the code.

--- caesar.c
+++ caesar.c
@@ -1,3 +1,13 @@
+/*
+ is a type of substitution cipher in which each letter in the plaintext is 'shifted' a certain number of places down the alphabet.  
+ For example, with a shift of 1, A would be replaced by B, B would become C, and so on.  
+ plaintext:  defend the east wall of the castle
+ ciphertext: efgfoe uif fbtu xbmm pg uif dbtumf
+ Formula: c[i] = (p[i] + key) % 26;
+ c: ciphertext
+ p: plaintext
+*/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
