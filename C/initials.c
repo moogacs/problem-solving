94c7422d13738e49cc6b37693a09898cd42045bd
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2018-10-07 10:02:13 +0500

    Create initials.c
    This code capitalizes the initials of a string and prints them to the screen.

---
+++ C/initials.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+int main (void)
+{
+    char name[MAX];
+    fgets(name, sizeof(name), stdin);
+    
+    printf("%c", toupper(name[0]));
+    
+    for (int i = 1; name[i] != '\0'; i++)
+    {
+        if (name[i] == ' ')
+        {
+            printf("%c", toupper(name[i + 1]));
+        }
+    }
+    
+    printf("\n");
+}
