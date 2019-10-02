ee6f05be2389a5d1a659e29dc176307e82a9b25f
ManavpreetSingh <42695610+ManavpreetSingh@users.noreply.github.com>
2019-10-02 09:25:16 +0530

    Update turbo_sort.c

--- C/turbo_sort.c
+++ C/turbo_sort.c
@@ -1,3 +1,14 @@
+/*
+Given the list of numbers, you are to sort them in non decreasing order.
+
+Input
+t – the number of numbers in list, then t lines follow [t <= 10^6].
+Each line contains one integer: N [0 <= N <= 10^6]
+
+Output
+Output given numbers in non decreasing order. */
+
+
 #include<stdio.h>
 
 int arr[1000000] = {0};
