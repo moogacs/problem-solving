e4aa60d9703f85db6f2393ec8c5c7b4ff36f4cf1
crowlkats <crowlkats@gmail.com>
2019-10-03 22:57:40 +0200

    Add descriptions

--- JavaScript/caesar.js
+++ JavaScript/caesar.js
@@ -1,3 +1,14 @@
+/*
+* is a type of substitution cipher in which each letter in the plaintext is 'shifted' a certain number of places down the alphabet.
+* For example, with a shift of 1, A would be replaced by B, B would become C, and so on.
+* plaintext:  defend the east wall of the castle
+* ciphertext: efgfoe uif fbtu xbmm pg uif dbtumf
+* Formula: c[i] = (p[i] + key) % 26;
+* c: ciphertext
+* p: plaintext
+* */
+
+
 //mod to wrap around to work with negative numbers as well
 const mod = (n, amount) => ((n % amount) + amount) % amount;
 
