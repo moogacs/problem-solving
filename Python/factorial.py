381beb2e6b574b95e05b6c63db9478c6caca1a98
Davit Tovmasyan <davitovmasyan@gmail.com>
2019-10-04 11:37:55 +0400

    Add description to factorial

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,3 +1,11 @@
+"""
+Factorial of a number n is product of all positive non-zero numbers less than or equal to n. 
+The factorial of n is denoted by n!. The (!) symbol is used to denote a factorial. 
+Factorial of any number can be represented as product of the number and factorial of a number less than it by one. 
+It can be written as, n! = n × (n−1)!
+"""
+
+
 import doctest
 
 
