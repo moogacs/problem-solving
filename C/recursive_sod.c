daa4cf334a4a8d1291709485a2132f6216ad94f3
Naveen <55792692+navi532@users.noreply.github.com>
2019-10-06 16:23:35 +0530

    Update recursive_sod.c

--- C/recursive_sod.c
+++ C/recursive_sod.c
@@ -1,4 +1,8 @@
 //Recursive method to  find sum of digits of a number
+//Prints a sum of digits of a number. For example
+//For Input num=123 
+//Output Sum of its digits=6
+//Similarly for Input=111 Output=3
 #include<stdio.h>
 int rsod(int);
 void main()
