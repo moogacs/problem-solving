d4ad3800424b31d7187e2186a6c496f131a1004b
harshrey <sadraymash@gmail.com>
2019-10-16 00:28:13 +0530

    Create sin_x_series.c
    Sin x is a series function and can be expanded upto infinite terms, using the Taylor's series expansion. This code finds the value of sin x upto a given limit.

---
+++ C/sin_x_series.c
@@ -0,0 +1,55 @@
+/* Sin x is a series function and can be expanded upto infinite terms.
+ Given code is used to find the value Sin x upto the desired limit, using the Taylor's series expansion*/
+ 
+#include<stdio.h>
+
+int factorial(int x);
+float power(float fx, int fj);
+
+int main()
+{
+	float x,x1,sum=0.0f;
+	int i,lim,j;
+
+	printf("\n\t Sin X series: ");
+    printf("\n\t Enter X (deg): ")
+	scanf("%f",&x);
+
+	printf("Enter the limit upto which you want to expand the series: ");
+	scanf("%d",&lim);
+    x1=x;
+	x = x*(3.1415/180);
+
+	for(i=1;i<=limit;i++)
+	{
+        j=2*i-1;
+		if(i%2!=0)
+		{
+			sum=sum+power(x,j)/factorial(j);
+		}
+		else
+			sum=sum-power(x,j)/factorial(j);
+	}
+
+	printf("Sin(%0.1f) = %f",x1,sum);
+	return 0;
+}
+int factorial(int fj)
+{
+	int i,fac = 1 ;
+	for(i = 1 ; i <= fj ; i++)
+		{
+            fac = fac * i ;
+        }
+	return fac;
+}
+float power(float fx, int fj)
+{
+    float pow = 1.0;
+    int i=0;
+    for(i=0;i<fj;i++)
+    {
+        pow = fx * pow ;
+    }
+    return pow ;
+}
