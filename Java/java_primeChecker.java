ad009ebea7c7462a1e33e6a6785436a4f606a5ea
Ram kumar <29110677+Ramkumar96@users.noreply.github.com>
2019-10-21 14:17:03 +0530

    Java Prime Checker
    Hackerank solve

---
+++ Java/java_primeChecker.java
@@ -0,0 +1,29 @@
+import static java.lang.System.in;
+
+class Prime {
+    void checkPrime(int... numbers) {
+        for (int num : numbers) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+    
+    boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }     
+}
