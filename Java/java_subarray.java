a222078c08b2db7e94d23d178a5cddce54dabd0e
Ram kumar <29110677+Ramkumar96@users.noreply.github.com>
2019-10-21 14:14:34 +0530

    Java Sub Array
    HAckerank solved

---
+++ Java/java_subarray.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+
+public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] array1= new int[n];
+    for(int i=0;i<n;i++){
+        array1[i]=sc.nextInt();
+    }
+    int count=0;
+    for(int j=0;j<n;j++){
+        int sum=0;
+        for(int k=j;k<n;k++){
+            sum=array1[k]+sum;
+            if(sum<0){
+                count++;
+            }
+        }
+    }
+    System.out.println(count);
+}
+}
